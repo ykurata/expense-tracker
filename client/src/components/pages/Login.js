e8f799c76e3f211875b19393eef4d73afcbd692f
ykurata <yasukokurata@gmail.com>
2020-06-04 16:23:13 -0400

    added login page

---
+++ client/src/components/pages/Login.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import image from '../images/login7.jpg';
+
+const loginStyles = {
+  backgroundImage: `url(${image})`,
+  backgroundSize: 'cover',
+  height: '100vh',
+  width: '100%',
+  position: 'relative',
+  display: 'table',
+  backgroundRepeat: 'no-repeat',
+}
+
+const Login = () => {
+	return (
+		<div id="login">
+			<div className="row"> 
+				<div className="col-lg-6 col-md-6 d-none d-md-block" style={loginStyles}></div>
+				<div className="col-lg-6 col-md-6">
+					<form>
+						<h2 className="title text-center mb-5">Log In</h2>
+						<div className="form-group">
+							<input type="email" className="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+						</div>
+						<div className="form-group">
+							<input type="password" className="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password"/>
+						</div>
+						<button type="button" className="btn btn-primary btn-lg btn-block">Log In</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Login;
