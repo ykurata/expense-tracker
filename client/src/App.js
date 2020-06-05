a50da1f7d4292f0f239d22d71787eeaedf542361
ykurata <yasukokurata@gmail.com>
2020-06-05 16:05:17 -0400

    added example sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Login from  './components/pages/Login';
 import SignUp from  './components/pages/SignUp';
+import Dashboard from  './components/pages/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Login}/>
+        <Route exact path="/" component={Dashboard}/>
+        <Route exact path="/login" component={Login}/>
         <Route exact path="/signup" component={SignUp}/>
       </Switch>
     </BrowserRouter>
