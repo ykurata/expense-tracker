d24b92cb2ec34169be68751f18d92b7f5c445d70
ykurata <yasukokurata@gmail.com>
2020-06-12 15:37:27 -0400

    added app.js and set up express

---
+++ server/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const path = require("path");
+const passport = require("passport");
+const cors = require('cors');
+const app = express();
+
+// import routes
+const user = require("./routes/user");
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.json());
+
+// Routes
+app.use("/user", user);
+
+// Set up cors
+app.use(cors());
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+// send 404 if no other route matched
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route Not Found',
+  });
+});
+  
+// setup a global error handler
+app.use((err, req, res, next) => {
+  if (enableGlobalErrorLogging) {
+    console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
+  }
+
+  res.status(500).json({
+    message: err.message,
+    error: process.env.NODE_ENV === 'production' ? {} : err,
+  });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+module.exports = app;
