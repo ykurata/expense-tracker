a288d968a8fdd2dd1b000ef568b2ea9583796c6e
ykurata <yasukokurata@gmail.com>
2020-06-13 23:03:38 -0400

    added create month route

--- server/routes/month.js
+++ server/routes/month.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-// import model
+
 const User = require('../models').User;
+const auth = require("./auth/auth");
 
-router.post("/", (req, res) => {
-
+router.post("/", auth, (req, res) => {
+  Month.create({
+    userId: req.user,
+    month: req.body.month,
+  })
+  .then(month => {
+    res.json(month);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 });
