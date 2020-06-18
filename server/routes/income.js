debea7cd600d447c7ff6418914d37541c41db4b2
ykurata <yasukokurata@gmail.com>
2020-06-18 17:38:38 -0400

    updated income routes with async/await

--- server/routes/income.js
+++ server/routes/income.js
@@ -5,61 +5,58 @@ const Income = require('../models').Income;
 const auth = require("./auth/auth");
 
 // Create a new income
-router.post("/", auth, (req, res) => {
-    Income.create({
+router.post("/", auth, async(req, res) => {
+  try { 
+    const income  = {
       userId: req.user,
       date: req.body.date,
       amount: req.body.amount,
       description: req.body.description
-    })
-    .then(income => {
-      res.json(income);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-	});
+    }
+    const newIncome = await Income.create(income);
+    return res.status(200).json(newIncome);
+  } catch(err) {
+    res.status(400).json({ error: err });
+  }
+});
 	
-	// Update an income 
-	router.put("/update/:id", auth, (req, res) => {
-		Income.findOne({ where: {id: req.params.id}})
-			.then(income => {
-				income.update({
-					date: req.body.date,
-					amount: req.body.amount,
-					description: req.body.description
-				})
-				res.status(200).json(income);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	});
-  
-  // Get all incomes
-  router.get("/all", auth, (req, res) => {
-    Income.findAll({
-      order: [["createdAt", "DESC"]]
-    })
-    .then(incomes => {
-      res.json(incomes);
-    })
-    .catch(err => {
-      console.log(err);
+// Update an income 
+router.put("/update/:id", auth, async(req, res) => {
+  try {
+    const income = await Income.findOne({ where: {id: req.params.id}});
+    const updatedIncome = await income.update({
+      date: req.body.date,
+      amount: req.body.amount,
+      description: req.body.description
     });
-  });
+    return res.status(200).json(updatedIncome);
+  } catch (err) {
+    res.status(400).json({ error: err });
+  }
+});
   
+// Get all incomes
+router.get("/all", auth, async(req, res) => {
+  try {
+    const incomes = await Income.findAll({
+      order: [["createdAt", "DESC"]]
+    });
+    return res.status(200).json(incomes);
+  } catch(err) {
+    res.status(400).json({ error: err });
+  }
+});
   
-  // Delete an income
-  router.delete("/delete/:id", auth, (req, res) => {
-    Income.findOne({ where: { id: req.params.id }})
-      .then(income => {
-        income.destroy();
-        return res.json({ message: "Successfully deleted"});
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-  
-  module.exports = router;
+
+// Delete an income
+router.delete("/delete/:id", auth, async(req, res) => {
+  try {
+    const income = await Income.findOne({ where: { id: req.params.id }});
+    await income.destroy();
+    return res.json({ message: "Deleted Income" });
+  } catch(err) {
+    res.status(400).json({ error: err });
+  }
+});
+
+module.exports = router;
