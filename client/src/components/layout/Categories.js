f156c45d5c1052e2026637b9ef52c51a75636aea
ykurata <yasukokurata@gmail.com>
2020-07-07 16:22:44 -0400

    added modal button and display expense details of each cards

--- client/src/components/layout/Categories.js
+++ client/src/components/layout/Categories.js
@@ -4,14 +4,12 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
 
 import { makeStyles } from '@material-ui/core/styles';
+import { ExpansionPanelDetails } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
-  card: {
-    height: '200px',
-    overflow: 'auto'
-  },
   totalAmount: {
     color: '#647fe3'
   },
@@ -31,6 +29,18 @@ const useStyles = makeStyles(theme => ({
 
 const Category = (props) => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState();
+
+  const handleOpen = (e) => {
+    setOpen(true);
+    const { myValue } = e.currentTarget.dataset;
+    setIndex(myValue);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   
   let holder = {};
 
@@ -48,35 +58,42 @@ const Category = (props) => {
     fixedExp.push({ name: prop, value: holder[prop].toFixed(2) });
   }
    
-  let filteredData = [];
-  for (let i = 0; i < fixedExp.length; i++ ) {
-    filteredData.push(props.data.filter(x => x.category.includes(fixedExp[i].name)));
-  }
+  // let filteredData = [];
+  // for (let i = 0; i < fixedExp.length; i++ ) {
+  //   filteredData.push(props.data.filter(x => x.category.includes(fixedExp[i].name)));
+  // }
 
   const card = fixedExp.map((item, index) => 
-    <Grid item xs={6} sm={6} md={4} key={index}>
-      <Card className={classes.card}>
+    <Grid item xs={6} sm={6} md={4} key={index} >
+      <Card>
         <CardContent>
           <Typography variant="body1" >{item.name}</Typography>
           <Typography variant="h6" className={classes.totalAmount}>${item.value}</Typography>
+          <button type="button" onClick={handleOpen} data-my-value={item.name}>
+            Open Modal
+          </button>
         </CardContent>
-        <CardContent>
-        {filteredData[index].map((x, i) => 
-            <div key={i}>
-              <Typography className={classes.description} variant='subtitle1'>{x.description}</Typography>
-              <div className={classes.amount}>
-                <Typography>${x.amount.toFixed(2)}</Typography>
-              </div>
-              <div className={classes.date}>
-              <Typography><Moment format="YYYY/MM/DD">{x.date}</Moment></Typography>
-              </div>  
-            </div>
-          )}
-        </CardContent>
-          
       </Card>  
     </Grid>
-  );
+  );  
+
+  const details = props.data.filter(x => x.category.includes(index));
+ 
+  let modalCard = details.map((x, i) => 
+                      <div key={i}>
+                        <Typography className={classes.description} variant='subtitle1'>{x.description}</Typography>
+                        <div className={classes.amount}>
+                          <Typography>${x.amount.toFixed(2)}</Typography>
+                        </div>
+                        <div className={classes.date}>
+                        <Typography><Moment format="YYYY/MM/DD">{x.date}</Moment></Typography>
+                        </div>  
+                      </div>
+                  )
+                        
+  
+  
+                
   
   return (
     <Grid container spacing={3}>
@@ -84,6 +101,19 @@ const Category = (props) => {
         <Typography className={classes.cateroryTitle} variant="h5">Expenses by Categories</Typography>
       </Grid>
       {card}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <Card>
+          <CardContent>
+            {modalCard}
+          </CardContent>
+        </Card>                
+      </Modal>    
+      
     </Grid>
   );
 }
