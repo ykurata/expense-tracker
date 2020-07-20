064b943919c24077b38724f0513a0c8a46b99235
ykurata <yasukokurata@gmail.com>
2020-07-20 10:41:06 -0400

    added modal button on dughnut chart

--- client/src/components/DoughnutChart.js
+++ client/src/components/DoughnutChart.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+
+import Modal from '@material-ui/core/Modal';
 
 import { Doughnut } from 'react-chartjs-2';
 
@@ -12,11 +15,48 @@ const useStyles = makeStyles(theme => ({
 	},
 	subTitle: {
 		padding: theme.spacing(1),
-	}
+  },
+  incomeButton: {
+    backgroundColor: '#3590F3',
+  },
+  expenseButton: {
+    backgroundColor: '#62BFED'
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalPaper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '.5px solid #000',
+    // boxShadow: theme.shadows[5],
+    // padding: theme.spacing(2, 4, 3),
+  },
 }));
 
 const DoughnutChart = (props) => {
   const classes = useStyles();
+  const [incomeOpen, setIncomeOpen] = useState(false);
+  const [expenseOpen, setExpenseOpen] = useState(false);
+
+  const handleIncomeOpen = () => {
+    setIncomeOpen(true);
+  };
+
+  const handleIncomeClose = () => {
+    setIncomeOpen(false);
+  };
+
+  const handleExpenseOpen = () => {
+    setExpenseOpen(true);
+  };
+
+  const handleExpenseClose = () => {
+    setExpenseOpen(false);
+  };
 
   const totalExpense = props.exp.reduce((a, {amount}) => a + amount, 0).toFixed(2);
   const totalIncome = props.inc.reduce((a, {amount}) => a + amount, 0).toFixed(2);
@@ -49,11 +89,39 @@ const DoughnutChart = (props) => {
       <Grid container className={classes.subTitle}>
         <Grid xs={6} item>
             <Typography variant="h6">${totalIncome}</Typography>
-            <Typography variant="body2">Income</Typography>
+            <Button onClick={handleIncomeOpen} className={classes.incomeButton} variant="contained" color="primary"><Typography variant="body2">Income</Typography></Button>
+            <Modal
+              open={incomeOpen}
+              onClose={handleIncomeClose}
+              className={classes.modal}
+              aria-labelledby="simple-modal-title"
+              aria-describedby="simple-modal-description"
+            >
+              <div className={classes.modalPaper}>
+                <h2 id="simple-modal-title">Text in a modal</h2>
+                <p id="simple-modal-description">
+                  Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                </p>
+              </div>
+            </Modal>
         </Grid>
-        <Grid xs={6} item>
+        <Grid xs={6} item align='right'>
           <Typography align='right' variant="h6">${totalExpense}</Typography>
-          <Typography align='right' variant="body2">Expenses</Typography>
+          <Button onClick={handleExpenseOpen} className={classes.expenseButton} variant="contained" color="primary"><Typography variant="body2">Expense</Typography></Button>
+          <Modal
+            open={expenseOpen}
+            onClose={handleExpenseClose}
+            className={classes.modal}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+          >
+            <div className={classes.modalPaper}>
+              <h2 id="simple-modal-title">Text in a modal</h2>
+              <p id="simple-modal-description">
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </p>
+            </div>
+          </Modal>
         </Grid>
       </Grid>
       {props.exp.length === 0 && props.inc.length === 0 ? (
